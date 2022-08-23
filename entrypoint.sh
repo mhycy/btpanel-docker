e100b5f9885113ce52b3ff12c4a5c9df6756a689
mhycy <i@mhycy.me>
2022-08-23 23:01:26 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@ pkill crond
 /usr/sbin/sshd -D &
 /usr/sbin/crond
 
+# 修复 mysql 权限问题
+if [[ -d /www/server/data ]]; then
+    chown -R mysql:mysql /www/server/data
+fi
+
 for file in /etc/init.d/*; do
     if [ -x $file ]; then
         $file restart
